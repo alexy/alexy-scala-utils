4cbfc8a623bbf489f86e6fa5e80201b213726766
Alexy Khrabrov <deliverable@gmail.com>
2009-08-25 14:01:03 -0400

    Debug and Info

--- util/src/main/scala/org/suffix/util/Info.scala
+++ util/src/main/scala/org/suffix/util/Info.scala
@@ -3,6 +3,10 @@ package org.suffix.util
 import System.err
 
 object Info {
+  val info = System.err.println(_ : Any)
+}
+
+object Debug {
   def print(show: Boolean)(s: String): Unit   = if (show) err.print(s)
   def println(show: Boolean)(s: String): Unit = if (show) err.println(s)
 }
